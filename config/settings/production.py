f4b5ad11962fdb6ee43552712f545e7190c79ec7
fromsian <fromSian@163.com>
2024-08-06 13:31:42 +0800

    feat: supabase database

--- config/settings/production.py
+++ config/settings/production.py
@@ -28,23 +28,23 @@ CACHES = {
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "postgres",
-#         "USER": "postgres.hrcpfvwjbsrmgoaivjff",
-#         "PASSWORD": os.environ.get("PGSQL_PWD"),
-#         "HOST": "aws-0-ap-southeast-1.pooler.supabase.com",
-#         "PORT": "6543",
-#     }
-# }
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres.hrcpfvwjbsrmgoaivjff",
+        "PASSWORD": os.environ.get("PGSQL_PWD"),
+        "HOST": "aws-0-ap-southeast-1.pooler.supabase.com",
+        "PORT": "6543",
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 
 GOOGLE_OAUTH2_REDIRECT_SUCCESS_URL = (
